7d0ec983c6b477c79d8e12af5cfeb43f9801bd34
Phillip Wong <deepelegant99@gmail.com>
2021-07-16 20:35:01 -0700

    Added a search bar that output to console

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import "./App.css";
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
+import React, { useState } from "react";
 
 function App() {
+  // eslint-disable-next-line
+  const [inputText, setInputText] = useState("");
+  // eslint-disable-next-line
+  const [todos, setTodos] = useState([]);
+
   return (
     <div className="App">
       <header>
-        <h1>Ed's Todo List</h1>
+        <h1>Phillip's Todo List</h1>
       </header>
+      <Form todos={todos} setTodos={setTodos} setInputText={setInputText} />
+      <TodoList />
     </div>
   );
 }
