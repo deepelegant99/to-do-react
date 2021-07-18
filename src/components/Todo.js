603076e0eda4cce0af3e45b8ca8a0326b0a0b32f
Phillip Wong <deepelegant99@gmail.com>
2021-07-18 15:02:14 -0700

    Added a dummy to do function that print something to console

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,13 +1,18 @@
 import React from "react";
 
 const Todo = ({text}) => {
+
+  const deleteHandler = () =>{
+    console.log("I am deleted")
+  }
+
   return (
     <div className="todo">
       <li className="todo-item">{text}</li>
       <button className="complete-btn">
         <i className="fas fa-check"></i>
       </button>
-      <button className="complete-btn">
+      <button onClick={deleteHandler} className="complete-btn">
         <i className="fas fa-trash"></i>
       </button>
     </div>
